529006c72f39bf29c3f0ab05521ae191d1b5ca2c
victor martinez simon <victormartinezsimon@gmail.com>
2019-10-17 01:00:37 +0200

    Set name to test instead test_mediatonic

---
+++ Test/Assets/Scripts/GameScene/PlaneGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneGenerator : MonoBehaviour
+{
+	public StartManager _startManager;
+	private GameManager _manager;
+	private PoolManager _pool;
+	private int _currentPlanesAlive;
+
+	void Start()
+	{
+		_manager = GameManager._instance;
+        _pool = GetComponent<PoolManager>();
+        _startManager = FindObjectOfType<StartManager>();
+
+        Random.InitState(42);
+		GeneratePlanes();
+	}
+
+	public void PlaneDestroyed()
+	{
+		//we notify the gamemanager
+		 GameManager._instance.PlaneHit();
+
+		 //we update the current planes alive
+		 --_currentPlanesAlive;
+
+		 if(_currentPlanesAlive == 0)
+		 {
+		 	GeneratePlanes();
+		 }
+	}
+
+	public void GeneratePlanes()
+	{
+        _currentPlanesAlive = Random.Range(3,5);
+
+        float _total_diff = _startManager.TopRight.y - _startManager.BottomLeft.y;
+
+        float _diff_between_planes = _total_diff / 10;
+
+        float first_plane_y = _startManager.BottomLeft.y + _total_diff / 2;
+
+        for (int i = 0; i < _currentPlanesAlive; ++i)
+		{
+			GameObject plane;
+			if(_pool.getItem(out plane))
+			{
+                Vector2 position = new Vector2(_startManager.BottomLeft.x, first_plane_y + _diff_between_planes * i);
+                plane.transform.position = position;
+                plane.SetActive(true);
+                plane.GetComponent<LinearMovement>()._velocity = Random.Range(2, 5);
+            }
+		}
+    }
+}
---
+++ Test/Assets/Scripts/GameScene/StartManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour
+{
+    private GameManager _manager;
+    public GameObject _left;
+    public GameObject _right;
+    public GameObject _top;
+    public GameObject _bottom;
+    public Camera _camera;
+    public GameObject _gun;
+
+    private Vector2 _topRight;
+    private Vector2 _bottomLeft;
+
+    public Vector2 TopRight
+    {
+        get { return _topRight; }
+    }
+
+    public Vector2 BottomLeft
+    {
+        get { return _bottomLeft;}
+    }
+
+    void Start()
+    {
+        _manager = FindObjectOfType<GameManager>();
+
+        Vector3 copyBottomLeft = _camera.ViewportToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane));
+        Vector3 copyTopRight = _camera.ViewportToWorldPoint(new Vector3(1, 1, _camera.nearClipPlane));
+
+        _topRight = new Vector2(copyTopRight.x, copyTopRight.y);
+        _bottomLeft = new Vector2(copyBottomLeft.x, copyBottomLeft.y);
+
+        {
+            //left
+            Vector2 position = new Vector2(_bottomLeft.x - 1, 0);
+            _left.transform.position = position;
+        }
+
+        {
+            //right
+            Vector2 position = new Vector2(_topRight.x + 1, 0);
+            _right.transform.position = position;
+        }
+
+        {
+            //top
+            Vector2 position = new Vector2(0, _topRight.y + 1);
+            _top.transform.position = position;
+        }
+
+        {
+            //bottom
+            Vector2 position = new Vector2(0, _bottomLeft.y -1);
+            _bottom.transform.position = position;
+        }
+
+        {
+            //guns
+            float diffX = _topRight.x - _bottomLeft.x;
+            float diffY = _topRight.y - _bottomLeft.y;
+
+            Vector2 position = new Vector2(0, 0);
+
+            position.x = _bottomLeft.x + diffX / 4;
+            position.y = _bottomLeft.y + diffY / 4;
+
+            _gun.transform.position = position;
+
+        }
+
+        _manager.StartGame();
+    }
+}
